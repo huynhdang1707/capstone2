7668f459605431e14fc62c1212ef52790e81fc31
Dang <huynhdang1707@gmail.com>
2023-11-23 00:47:47 +0700

    update

---
+++ CAPSTONE2/index.js
@@ -0,0 +1,14 @@
+var dssp =[];
+function themSP(){
+    var _tenSP = document.getElementById("tenSP").value;
+    var _giaSP= document.getElementById("giaSP").value *1;
+    var _hinhSP = document.getElementById("hinhSP").value ;
+
+var sp ={
+    tenSP : _tenSP,
+    giaSP : _giaSP,
+    hinhSP : _hinhSP,
+
+    }
+}
+
